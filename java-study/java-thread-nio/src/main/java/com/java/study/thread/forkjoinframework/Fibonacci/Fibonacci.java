bad699bcfdd01c95dd98720862e61e7a4d157e8e
yjydmlh <yjydmlh@gmail.com>
2016-07-07 23:16:47 +0800

    计算斐波那契数列

--- java-study/java-thread-nio/src/main/java/com/java/study/thread/forkjoinframework/Fibonacci/Fibonacci.java
+++ java-study/java-thread-nio/src/main/java/com/java/study/thread/forkjoinframework/Fibonacci/Fibonacci.java
@@ -1,9 +1,19 @@
 package com.java.study.thread.forkjoinframework.Fibonacci;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+
 public class Fibonacci {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    	ForkJoinPool pool = new ForkJoinPool();
+    	for(int i=1;i<=10;i++){
+    		FibonacciTask task = new FibonacciTask(i);
+    		Future<Integer> future = pool.submit(task);
+    		Integer rs = future.get();
+    		System.out.println(rs);
+    	}
     }
 
 }
--- java-study/java-thread-nio/src/main/java/com/java/study/thread/forkjoinframework/Fibonacci/FibonacciTask.java
+++ java-study/java-thread-nio/src/main/java/com/java/study/thread/forkjoinframework/Fibonacci/FibonacciTask.java
@@ -2,17 +2,37 @@ package com.java.study.thread.forkjoinframework.Fibonacci;
 
 import java.util.concurrent.RecursiveTask;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class FibonacciTask extends RecursiveTask<Integer> {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -911034629955502001L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -911034629955502001L;
 
-    @Override
-    protected Integer compute() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	private int n;
+
+	public FibonacciTask(int n) {
+		this.n=n;
+	}
+
+	@Override
+	protected Integer compute() {
+		int rs = 0;
+		if(n<=1){
+			rs=1;
+		}else{
+			FibonacciTask left = new FibonacciTask(n-1);
+			FibonacciTask right = new FibonacciTask(n-1);
+			left.fork();
+			right.fork();
+			rs = left.join()+right.join();
+		}
+		return rs;
+	}
 
 }
