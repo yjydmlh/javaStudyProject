4a0a19b31c37f8d8f298551d7fdd701e14d0ff1e
yjydmlh <yjydmlh@gmail.com>
2016-08-24 16:33:45 +0800

    作业题

--- java-study/java-courses/src/main/java/org/java/courses/leaderus/Class2.java
+++ java-study/java-courses/src/main/java/org/java/courses/leaderus/Class2.java
@@ -1,7 +1,9 @@
 package org.java.courses.leaderus;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,7 +18,8 @@ import lombok.Setter;
 public class Class2 {
 
     public static void main(String[] args) {
-        
+        generate();
+//        System.out.println(randomStr(5));
     }
 
     public static void generate(){
@@ -24,8 +27,45 @@ public class Class2 {
         Salary s = null;
         for(int i=0;i<10000;i++){
             s = new Salary();
-//            s.setBonus(bonus);
+            s.setBonus(random(0,100000));
+            s.setBaseSalary(random(5,1000000));
+            s.setName(randomStr(5));
+            l.add(s);
         }
+        l.sort(new Comparator<Salary>() {
+
+            @Override
+            public int compare(Salary o1, Salary o2) {
+                int s1 = o1.getBaseSalary()*13+o1.getBonus();
+                int s2 = o2.getBaseSalary()*13+o2.getBonus();
+                if(s1==s2){
+                    return 0;
+                }else if(s1>s2){
+                    return -1;
+                }
+                return 1;
+            }
+        });
+        System.out.println(l.subList(0, 10));
+    }
+    
+    
+    public static String randomStr(int n) {
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<n;i++){
+            int t = random(97,122);
+            sb.append((char)t);
+        }
+        return sb.toString();
+    }
+
+    public static int random(int down,int up){
+        Random rand = new Random();
+        int r = rand.nextInt(up);
+        if(r>=down){
+            return r;
+        }
+        return r+down > up ?up:r+down;
     }
     
 }
@@ -34,4 +74,11 @@ public class Class2 {
 class Salary{
     private String name;
     private int bonus;
+    private int baseSalary;
+    
+    @Override
+    public String toString(){
+        return  "Salary(name="+name+",bonus="+bonus+",baseSalary="+baseSalary+",sum="+(baseSalary*13+bonus)+")";
+    }
+    
 }
