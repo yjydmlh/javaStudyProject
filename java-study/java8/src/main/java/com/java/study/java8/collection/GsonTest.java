96b59a6e10f8c276f8ce16e4ec83ba1c4ec14fe3
yjydmlh <yjydmlh@gmail.com>
2023-12-06 22:33:22 +0800

    一些测试

---
+++ java-study/java8/src/main/java/com/java/study/java8/collection/GsonTest.java
@@ -0,0 +1,10 @@
+package com.java.study.java8.collection;
+
+import com.google.gson.Gson;
+
+public class GsonTest {
+
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+    }
+}
---
+++ java-study/java8/src/main/java/com/java/study/java8/collection/JacksonTest.java
@@ -0,0 +1,13 @@
+package com.java.study.java8.collection;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JacksonTest {
+
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+    }
+
+
+}
