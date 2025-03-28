adf27645c4739059dd6d81b5f04344cefbaee490
yjydmlh <yjydmlh@gmail.com>
2025-03-29 01:32:17 +0800

    gson util

--- java-study/java8/src/main/java/com/java/study/java8/gson/GsonTest.java
+++ java-study/java8/src/main/java/com/java/study/java8/gson/GsonTest.java
@@ -3,7 +3,14 @@ package com.java.study.java8.gson;
 public class GsonTest {
 
     public static void main(String[] args) {
-
+        Long bigLong = 9007199254740991L; // 2^53 - 1
+        String json = GsonUtil.toJson(bigLong);
+        System.out.println(json);
+        Long result = GsonUtil.fromJson(json, Long.class);
+        System.out.println(result);
+        Long result2 = GsonUtil.fromJson(Long.MAX_VALUE+"", Long.class);
+        System.out.println(result2);
+        System.out.println(GsonUtil.toJson(Long.MAX_VALUE));
     }
 
 }
